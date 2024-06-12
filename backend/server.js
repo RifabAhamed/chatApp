4f02336e402c1c0a4a2d293f5953b676f4ccafd1
RifabAhamed <rifabahamed@gmail.com>
2024-06-13 03:31:00 +0530

    created the database

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
-configDotenv.config();
-
+dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     //root route http://localhost:5000/
     res.send("Hello World!!");
 });
-app.use("/api/auth",authRoutes);
 
-app.get("/api/auth/signup", (req,res)=>{
-    console.log("signuproute");
-});
-app.get("/api/auth/login", (req,res)=>{
-    console.log("loginroute");
-});
+app.use("/api/auth", authRoutes);
 
 
-
-app.listen(PORT,()=> console.log('Server Running on Port ${PORT}'));
+app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
