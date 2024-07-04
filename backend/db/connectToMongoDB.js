53e9757f5ecd6c892c5b71f3ddcd1080938be988
RifabAhamed <rifabahamed@gmail.com>
2024-07-05 01:16:02 +0530

    bug solved and installed pacakge json for backend

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-
 const connectToMongoDB = async() =>{
     try{
+        console.log("process.env.MONGO_DB_URI",process.env.MONGO_DB_URI)
         await mongoose.connect(process.env.MONGO_DB_URI);
         console.log("connected to MongoDB");
     }
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,23 @@
+import 'dotenv/config';
 import express from "express";
-import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-dotenv.config();
+// Middleware to parse JSON
+app.use(express.json());
 
-app.use(express.json()); //
-
+// Use auth routes
 app.use("/api/auth", authRoutes);
 
+// Optionally, you can enable a root route
 // app.get("/", (req, res) => {
-//     //root route http://localhost:5000/
 //     res.send("Hello World!!");
 // });
 
-
-
 app.listen(PORT, () => {
     connectToMongoDB();
-    console.log('Server Running in port ${PORT}');
-    });
+    console.log(`Server running on port ${PORT}`);
+});
