257ffb57de4550195cfb076e4f0340843ff05c9d
RifabAhamed <rifabahamed@gmail.com>
2024-07-07 19:33:53 +0530

    Continuo with the sendMessage function

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import 'dotenv/config';
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,8 +11,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware to parse JSON
 app.use(express.json());
 
+// Middleware to parse cookies
+app.use(cookieParser());
+
 // Use auth routes
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // Optionally, you can enable a root route
 // app.get("/", (req, res) => {
