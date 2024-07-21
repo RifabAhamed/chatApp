28dd331dbf2be0257d31b54762cec99d6d08b5f1
RifabAhamed <rifabahamed@gmail.com>
2024-07-21 14:17:21 +0530

    COmpleted the basic backend and frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-import Login from './pages/Login/Login'
-import SignUp from './pages/Signup/SignUp'
+import Home from './pages/home/Home.jsx'
+import Login from './pages/Login/Login.jsx'
+import SignUp from './pages/Signup/SignUp.jsx'
 
 function App() {
   return <div className='p-4 h-screen flex items-center justify-center'>
-    <SignUp/> 
+    <Home/> 
   </div>
 }
 
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../../components/sidebar/Sidebar.jsx";
+import MessageContainer from "../../components/messages/MessageContainer.jsx";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="flex sm:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+      <Sidebar />
+      <MessageContainer/>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
