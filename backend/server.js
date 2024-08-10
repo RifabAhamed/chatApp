c177c84dea7267b9581ac172644d932cd8cdf5e1
RifabAhamed <rifabahamed@gmail.com>
2024-08-10 23:21:41 +0530

    inline status setted

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,12 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from 'cookie-parser';
+import {app, server} from './socket/socket.js'
+import dotenv from "dotenv";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
+dotenv.config();
 // Middleware to parse JSON
 app.use(express.json());
 
@@ -25,7 +27,7 @@ app.use("/api/users", userRoutes)
 //     res.send("Hello World!!");
 // });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server running on port ${PORT}`);
 });
